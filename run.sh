0381d32085122d1e02e38a4b5fdbfc68c36ec554
MAELR5 <maelr5@ulaval.ca@ul-val-pr-ssh01.l.ul.ca>
2025-04-20 18:30:41 -0400

    resolve dataleakage

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ module load python/3.10
 source parksenv/bin/activate
 
 # rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/train_with_wav2vec2/base/1986
-# python ssl/trainwav2vec.py ssl/hparams_wav2vec.yaml --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=25
+# python ssl/trainwav2vec.py ssl/hparams_wav2vec.yaml --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=5
 
-rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/1986
+rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/full_dataset/fbank/1986
 python train_models/train_ecapatdnn_fbanks.py train_models/hparams_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0'

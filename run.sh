8b5cac159db94bc6c73142933ce43430f858bbbe
MAELR5 <maelr5@ulaval.ca@ul-val-pr-gpu05.l.ul.ca>
2025-04-25 22:56:47 -0400

    hyperparameter tuning

--- run.sh
+++ run.sh
@@ -10,11 +10,20 @@ source parksenv/bin/activate
 # python train_models/train_ecapatdnn_fbanks.py train_models/hparams_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
 
 
-rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch/phrases/fbank/1986
-# Run Training
-python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch_phrases_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+# rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch/phrases/fbank/1986
+# # Run Training
+# python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch_phrases_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
 
 
 
 # rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/wav2vecfs/full_dataset/1986
 # python train_models/train_wav2vec_ecapatdnn.py train_models/hparams_wav2vec_ecapatdnn.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+
+# rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch64/full_dataset/fbank/1986
+# # Run Training
+# python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch64_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+
+rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch32/full_dataset/fbank/1986
+# Run Training
+python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch32_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+
--- run2.sh
+++ run2.sh
@@ -7,8 +7,8 @@ source parksenv/bin/activate
 
 
 # ECAPATDNN from scratch
-rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch/full_dataset/fbank/1986
-python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+# rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch/full_dataset/fbank/1986
+# python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
 
 
 # ECAPATDNN + MLP Ensemble
@@ -20,4 +20,25 @@ python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch_e
 # rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch/augmentation/full_dataset/fbank/1986
 # python train_models/train_ecapatdnnaugmented_fbanks.py train_models/hparams_fromscratch_ecapatdnnaugmented_fbanks.yaml --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
 
+
+
+# rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch128/full_dataset/fbank/1986
+# # Run Training
+# python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch128_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+
+# rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch16/full_dataset/fbank/1986
+# # Run Training
+# python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch16_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+
+# rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch8/full_dataset/fbank/1986
+# # Run Training
+# python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch8_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+
+# rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch2/full_dataset/fbank/1986
+# # Run Training
+# python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch2_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
+
+rm -rf /home/ulaval.ca/maelr5/scratch/parkinsons-results/ECAPA-TDNN/fromscratch4/full_dataset/fbank/1986
+# Run Training
+python train_models/train_ecapatdnn_fbanks.py train_models/hparams_fromscratch4_ecapatdnn_fbanks.yaml  --data_folder='/home/ulaval.ca/maelr5/scratch/parkinsons' --device='cuda:0' --number_of_epochs=15 --use_tensorboard=True
 
